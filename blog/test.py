e858d5cbefc3ac57b336e52af44d3908dc5ee0b7
xjh <877637420@qq.com>
2016-12-08 11:05:26 +0800

    update

--- blog/test.py
+++ blog/test.py
@@ -1,25 +1,11 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
-django.setup()
+cs = [":","<",">","\\","/","?",'"',"*","|"]
+str = "job_1481246310720_0452-1481357273822-xiangy-Distributed+Lzo+Indexer+%5B%2FDATA%2FETL%2FTMP_SUBJECT_ADP-1481357300332-481-0-SUCCEEDED-root.xiangy-1481357284713.jhist"
+for c in cs:
+    if str.find(c)>0:
+        print(c)
+print(len(cs))
 
-import blog.models
-from blog.models import Article
 
-# create数据库增加操作
-# res1 = Article.objects.create(title = 'Hello World', category = 'Python', content = '我们来做一个简单的数据库增加操作')
-# res2 = blog.models.Person.objects.create(name="ok",age=13)
-
-# update
-article = Article.objects.get(id=1)
-article.title = "kill me inside"
-# article.save()
-
-# filter ignore case
-a2 = Article.objects.filter(title__icontains="world")
-print(a2)
-# a2[0].delete()
-
-res3 = Article.objects.all()
-print(res3)
