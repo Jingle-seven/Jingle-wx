ca7f65ced3a889d8c270cced221ec5fa48c9aafe
xjh <877637420@qq.com>
2017-04-06 14:57:25 +0800

    change EmailSeder

--- jingle-py/demo/net/EmailSender.py
+++ jingle-py/demo/net/EmailSender.py
@@ -2,28 +2,26 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-# 第三方 SMTP 服务
-mail_host="smtp.163.com"
-mail_user="13631435802@163.com"
-mail_pass="xz1994"   # 口令(网易授权码,与网易登录密码不同)
+# 口令(网易授权码,与网易登录密码不同)
+# 不知道为什么第一个账户发送不报错, 事后却收不到邮件
+acount1={"host":"smtp.163.com","user":"xiong_jin_hua@163.com","passwd":"jrqs1994"}
+acount2={"host":"smtp.163.com","user":"13631435802@163.com","passwd":"xz1994"}
 
-
-def sendEmail(subj="Subject",content="Blank",to="xiong_jinhua@foxmail.com",):
-    sender = '13631435802@163.com'
-    receivers = [to]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+def sendEmail(subj="Subject",content="Blank",to="xiong_jinhua@foxmail.com",acount=acount2):
     message = MIMEText(content, 'plain', 'utf-8')
     message['From'] = "荆人七十"
     message['To'] = to
-    subject = subj
-    message['Subject'] = Header(subject, 'utf-8')
+    message['Subject'] = Header(subj, 'utf-8')
     try:
         smtpObj = smtplib.SMTP()
-        smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
-        smtpObj.login(mail_user, mail_pass)
-        smtpObj.sendmail(sender, receivers, message.as_string())
+        smtpObj.connect(acount["host"], 25)  # 25 为 SMTP 端口号
+        smtpObj.login(acount["user"], acount["passwd"])
+        smtpObj.sendmail(acount["user"], [to, ], message.as_string())
+        print("发送成功")
     except smtplib.SMTPException as e:
         print("Error: 无法发送邮件")
         raise e
+    smtpObj.quit()
 
 if __name__ =="__main__":
     sendEmail()
