97627bdc3802623429d499522512200e6689e281
xjh <877637420@qq.com>
2017-05-12 17:13:11 +0800

    add matplotlibTest

---
+++ jingle-py/test/MatplotlibTest.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+
+from pylab import *
+
+n = 15
+X = np.arange(n)
+# Y1 = (1-X/float(n)) * np.random.uniform(0.5,1.0,n)
+Y1 = (1) * np.random.uniform(0.5,1.0,n)
+Y2 = (1-X/float(n)) * np.random.uniform(0.5,1.0,n)
+
+bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
+bar(X, -Y2, facecolor='#ff9999', edgecolor='black')
+
+for x,y in zip(X,Y1):
+    text(x+0.4, y+0.05, '%.2f' % y, ha='center', va= 'bottom')
+
+ylim(-1.25,+1.25)
+show()
