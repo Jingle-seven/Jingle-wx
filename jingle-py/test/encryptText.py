519e5c763e59342b7079d60ec1f4d82ad4ab955b
xjh <xiong_jinhua@foxmail.com>
2018-04-08 23:23:19 +0800

    change enc

--- jingle-py/test/encryptText.py
+++ jingle-py/test/encryptText.py
@@ -1,7 +1,7 @@
 # 加密文本，要求可逆
 # 初步思路为计算unicode码。
 # 将密码的几个unicode字符相与算出一个unicode密码，再将原有文本字符逐个与unicode密码进行计算
-# 不过存在有些字符加密后无法用utf-8保存的问题，待解决
+# 不过存在有些字符加密后无法用utf-8保存的问题，对于这些字符直接跳过
 
 
 def genPwd(rawPwd):
@@ -17,23 +17,21 @@ def encryptText(rawText ,uniPwd):
     for i in rawText:
         unic = ord(i)
         unic1 = unic ^ uniPwd
-        ciphertext.append(chr(unic1))
-        # 有些密文无法用utf-8编码，待解决
         try:
             chr(unic1).encode("utf-8")
         except Exception as e:
-            print(i,unic1)
             errChars.add(i)
-    print(errChars)
+            # 如果加密后无法用utf-8保存，保留原字符
+            unic1 = unic
+        ciphertext.append(chr(unic1))
+    print("error chars",errChars)
     return "".join(ciphertext)
 
-fileName = "挪威的森林简析"
+fileName = "挪威的森林简析-enc-enc"
 text = open('../resource/jieba/%s.txt'%fileName,encoding='utf8').read()
 file = open('../resource/jieba/%s-enc.txt'%fileName, "w", encoding="utf-8")
-pwd = "逗比"
+pwd = input("input password:")
 uniPwd = genPwd(pwd)
 secretText = encryptText(text,uniPwd)
-# print(text)
-# print(secretText)
-# file.write(secretText)
-print(encryptText(secretText,uniPwd))
+file.write(secretText)
+print("encrypt finished")
