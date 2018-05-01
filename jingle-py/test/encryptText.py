70e5a22c1279a49978620fba7f876a89af8bbc90
xjh <xiong_jinhua@foxmail.com>
2018-05-01 16:41:00 +0800

    change enc

--- jingle-py/test/encryptText.py
+++ jingle-py/test/encryptText.py
@@ -2,6 +2,7 @@
 # 初步思路为计算unicode码。
 # 将密码的几个unicode字符相与算出一个unicode密码，再将原有文本字符逐个与unicode密码进行计算
 # 不过存在有些字符加密后无法用utf-8保存的问题，对于这些字符直接跳过
+import sys
 
 
 def genPwd(rawPwd):
@@ -16,22 +17,44 @@ def encryptText(rawText ,uniPwd):
     errChars = set()
     for i in rawText:
         unic = ord(i)
-        unic1 = unic ^ uniPwd
+        unicEnc = unic ^ uniPwd
         try:
-            chr(unic1).encode("utf-8")
+            # 如果加密后无法用utf-8保存，保留原字符
+            chr(unicEnc).encode("utf-8")
+            if unicEnc==13:
+                raise Exception("不转换为换行符,因无法妥善保存")
         except Exception as e:
             errChars.add(i)
-            # 如果加密后无法用utf-8保存，保留原字符
-            unic1 = unic
-        ciphertext.append(chr(unic1))
-    print("error chars",errChars)
+            unicEnc = unic
+        # print("%s -> %s"%(i,chr(unicEnc)))
+        ciphertext.append(chr(unicEnc))
+    # print("error chars",errChars)
     return "".join(ciphertext)
 
-fileName = "挪威的森林简析-enc-enc"
-text = open('../resource/jieba/%s.txt'%fileName,encoding='utf8').read()
-file = open('../resource/jieba/%s-enc.txt'%fileName, "w", encoding="utf-8")
+
+
+def encFile(fileName,pwd):
+    text = open(fileName, encoding='utf8').read()
+    file = open('%s.enc' % fileName, "w", encoding="utf-8")
+    secretText = encryptText(text, genPwd(pwd))
+    file.write(secretText)
+    print("{0} was encrypted to {0}.enc".format(fileName))
+
+
+def encFileCmd():
+    try:
+        fileName = sys.argv[1]
+    except Exception as e:
+        fileName = input("input file name:")
+    try:
+        pwd = sys.argv[2]
+    except Exception as e:
+        pwd = input("input password:")
+    encFile(fileName, pwd)
+
+
+# encFileCmd()
+fileName = "weibo.sql"
 pwd = input("input password:")
-uniPwd = genPwd(pwd)
-secretText = encryptText(text,uniPwd)
-file.write(secretText)
-print("encrypt finished")
+encFile(fileName,pwd)
+encFile("%s.enc"%fileName,pwd)
