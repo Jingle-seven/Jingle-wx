5ec6de8081c870ffc7ee9556013b0a8f760d7337
jinhua <xiong_jinhua@foxmail.com>
2018-11-30 16:52:41 +0800

    update

--- jingle/demo/net/12306Requests.py
+++ jingle/demo/net/12306Requests.py
@@ -9,15 +9,20 @@ GUANG_ZHOU = "GZQ"
 GUANG_ZHOU_DONG = "GGQ"
 
 
-def getRawJson(date, fromStt=GUANG_ZHOU, toStt=NAN_XIONG):
-    ticketUrl = "https://kyfw.12306.cn/otn/leftTicket/query" \
+def getRawJson(date=time.strftime("%Y-%m-%d", time.localtime()), fromStt=GUANG_ZHOU, toStt=NAN_XIONG):
+    url = 'https://kyfw.12306.cn/otn/leftTicket/queryA' \
+          '?leftTicketDTO.train_date=2018-10-01' \
+          '&leftTicketDTO.from_station=GZQ' \
+          '&leftTicketDTO.to_station=NCQ' \
+          '&purpose_codes=ADULT'
+    ticketUrl = "https://kyfw.12306.cn/otn/leftTicket/queryA" \
                 "?leftTicketDTO.train_date=%s" \
                 "&leftTicketDTO.from_station=%s" \
                 "&leftTicketDTO.to_station=%s" \
                 "&purpose_codes=ADULT" % (date, fromStt, toStt)
 
     r2 = requests.get(ticketUrl, verify=False)
-    return r2.json()
+    return r2.json()['data']['result']
 
 
 def getTrainsInfo(rawJson):
@@ -47,8 +52,9 @@ def formatInfo(trainInfo):
     return info
 
 
-def watcher(trainNum=[]):
-    trainsInfo = getTrainsInfo(getRawJson(date="2017-05-27"))
+def watcher():
+    trainNum=[]
+    trainsInfo = getTrainsInfo(getRawJson())
     for i in trainsInfo:
         info = formatInfo(i)
         hardSeat = info.get("硬座", "--")
@@ -67,54 +73,10 @@ if __name__ == "__main__":
     #         watcher()
     #     except Exception as e:
     #         print("Xz Error: "+str(e))
-    watcher()
-    time.sleep(60* 10)
+    # watcher()
+    # time.sleep(60* 10)
+    print(getRawJson('2018-10-01'))
 
-    """{  
-        "train_no": "240000G14104",          // 列车编号  
-        "station_train_code": "G141",        // 车次  
-        "start_station_telecode": "VNP",     // 始发站编码  
-        "start_station_name": "北京南",      // 始发站名  
-        "end_station_telecode": "AOH",       // 终到站编码  
-        "end_station_name": "上海虹桥",      // 终到站名  
-        "from_station_telecode": "VNP",      // 查询输入经过站编码  
-        "from_station_name": "北京南",       // 查询输入经过站名  
-        "to_station_telecode": "AOH",        // 查询输入到站编码  
-        "to_station_name": "上海虹桥",       // 查询输入到站名  
-        "start_time": "14:16",               // 出发时间  
-        "arrive_time": "19:47",              // 到站时间  
-        "day_difference": "0",               // 花费天数  
-        "train_class_name": "",  
-        "lishi": "05:31",                    // 历时  
-        "canWebBuy": "Y",                    // 是否可以预定  
-        "lishiValue": "331",  
-        "yp_info": "O055300094M0933000999174800017",  
-        "control_train_day": "20301231",  
-        "start_train_date": "20140123",  
-        "seat_feature": "O3M393",  
-        "yp_ex": "O0M090",  
-        "train_seat_feature": "3",  
-        "seat_types": "OM9",  
-        "location_code": "P3",  
-        "from_station_no": "01",  
-        "to_station_no": "09",  
-        "control_day": 19,  
-        "sale_time": "1400",                // 出票时间点hhmm  
-        "is_support_card": "1",  
-        "gg_num": "--",  
-        "gr_num": "--",          // 高级软卧座剩余数  
-        "qt_num": "--",          // 其他座剩余数  
-        "rw_num": "--",          // 软卧座剩余数  
-        "rz_num": "--",          // 软座座剩余数  
-        "tz_num": "--",          // 特等座剩余数  
-        "wz_num": "--",          // 无座座剩余数  
-        "yb_num": "--",  
-        "yw_num": "--",          // 硬卧座剩余数  
-        "yz_num": "--",          // 硬座座剩余数  
-        "ze_num": "有",          // 二等座剩余数  
-        "zy_num": "有",          // 一等座剩余数  
-        "swz_num": "17"          // 商务座剩余数  
-    }"""
     
     # 接口返回值变更了,原本是json,现在是字符串
 "|预订|630000T1700M|T170|GZQ|SNH|GZQ|NCQ|14:55|18:31|03:36|N|qfbbFTUxvI2%2FyFOG0vJK4RzPanp2kXWj%2BPKpkHd9EXGvuLTcHF3zA2x%2BfeQ%3D|20170527|3|Q7|01|04|0|0||||无|||无||无|无||||10401030|1413"
