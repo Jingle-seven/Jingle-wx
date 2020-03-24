7b0f34c31ee1231fb261be315692462c68873d4f
xiong_E1471G <xiong_jinhua@foxmail.com>
2020-03-24 14:00:45 +0800

    update DelDuplicateFile.py

--- jingle/demo/DelDuplicateFile.py
+++ jingle/demo/DelDuplicateFile.py
@@ -1,12 +1,13 @@
 #coding=utf-8
-import hashlib
-import os
-path = "C:/Users/Administrator/Desktop/test/"
+import hashlib,os,re
+path = "D:/迅雷下载/画画/"
 
 fileList = os.listdir(path)
 fileMD5ToSize = {}
 delCounter = 0
+bookCounter = 0
 for fileName in fileList:
+    bookCounter +=1
     if os.path.isdir(path + fileName):continue #跳过目录
     midName = str(fileList.index(fileName))
     headBits = open(path + fileName, "rb").read(10000)
@@ -17,13 +18,24 @@ for fileName in fileList:
     # MD5一样，大小一样，说明是重复，删除之
     if fileMD5 in fileMD5ToSize and fileMD5ToSize[fileMD5] == fileSize:
         print('delete '+fileName)
-        os.remove(path + fileName)
+        # os.remove(path + fileName)
         delCounter +=1
+        continue
     else:
         fileMD5ToSize[fileMD5] = fileSize
-    if '、' in fileName:
-        newName = fileName.split('、')[1]
-        os.rename(path+fileName,path + newName)
+    # x = re.sub(r'[^\D]+\.pdf','',fileName)
+    # x = x.replace(" ","").replace("+","")
+    x = fileName.replace('.pdf.pdf','')
+    try:
+        fullName = path + x
+        # print(x)
+        if  '.pdf' not in fullName:
+            fullName = fullName + '.pdf'
+            print(fullName)
+        os.rename(path+fileName,fullName)
+    except FileExistsError as e:
+        print(x)
+        continue
 
 # print(fileMD5ToSize)
 print("delete %d files"% delCounter)
